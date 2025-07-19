b07fd388f2f7a9dabcd9c6c9724307a4e6507954
haseeb2412 <haseeb241202@gmail.com>
2025-07-20 03:20:33 +0500

    complete practice of factoryh design pattern

--- factory/DeveloperClient.java
+++ factory/DeveloperClient.java
@@ -1,3 +1,18 @@
+// advantages
+// focus on creating object for interface rather than implementation
+// loose coupling more robost code  ( dont  know where the object creating )
+
+
+// chatgpt
+// Creates objects without exposing the creation logic
+// Returns objects of a common parent class or interface
+// Helps achieve loose coupling between client and implementation classes
+// Makes the code more flexible, scalable, and easy to maintain
+// Allows easy addition of new object types with minimal code changes
+
+
+
+
 package factory;
 
 public class DeveloperClient {
